84fede494982c277ceb7ad50a4f070ca050be403
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-07-20 17:15:30 -0400

    Clean-up, no changes to code

--- Go_servers/routes/contacts.go
+++ Go_servers/routes/contacts.go
@@ -8,34 +8,26 @@ import (
 
 // Define a struct to hold the JSON data
 
-
 func ContactGetHand(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./htmlTemplates/contact.html"))
 	tmpl.Execute(w, nil)
 }
 
 func ContactPostHand(w http.ResponseWriter, r *http.Request) {
-	// tmpl := template.Must(template.ParseFiles("./htmlTemplates/services.html"))
-	   // Parse the form values
-	   err := r.ParseForm()
-	   if err != nil {
-		   http.Error(w, "Unable to parse form", http.StatusBadRequest)
-		   return
-	   }
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
 
 	tmpl:= template.Must(template.ParseFiles("./htmlTemplates/contact_sent.html"))
-	   // Access the form values
-	   name := r.FormValue("name")
-	   email := r.FormValue("email")
-	   message := r.FormValue("message")
-   
-	   // Print the form values
-	   fmt.Printf("Name: %s\nEmail: %s\nMessage: %s\n", name, email, message)
-   
-	   // Set the redirect header for HTMX
-	   tmpl.Execute(w, nil)
-	//    w.Header().Set("HX-Redirect", "/")
-   
-	   // Respond to the client
-	//    w.Write([]byte("Form submitted successfully!"))
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	message := r.FormValue("message")
+
+	// Print the form values for FUTURE IMPLEMENTATION HERE
+	fmt.Printf("Name: %s\nEmail: %s\nMessage: %s\n", name, email, message)
+
+	// Send the html content for htmx
+	tmpl.Execute(w, nil)
 }
