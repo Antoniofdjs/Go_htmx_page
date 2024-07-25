370b4e0aea27ef567d9204b3d529b9d4f19a6dfd
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-07-24 22:16:43 -0400

    UPDATE - Using a single map for all /works routes

--- Go_servers/handlers/work/works.go
+++ Go_servers/handlers/work/works.go
@@ -4,14 +4,33 @@ package work
 
 import (
 	"Go_servers/handlers/editors"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"text/template"
 )
 
-// Structs for json request
+
+var WorksMap = map[string][]Picture{
+	"Pictures": {
+		{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg", PicID: 1},
+		{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 2},
+		{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 3},
+		{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 4},
+		{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg",PicID: 5},
+		{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 6},
+		{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 7},
+		{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 8},
+		{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg", PicID: 9},
+		{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 10},
+		{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 11},
+		{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 12},
+	},
+}
+
 type RequestData struct {
     PicID      string `json:"PicID"`
     Option  string `json:"Option"`
@@ -19,7 +38,7 @@ type RequestData struct {
 }
 
 type Picture struct{
-	PicID uint16
+	PicID int
 	Title string
 	Path string
 }
@@ -32,22 +51,8 @@ type PictureData struct {
 
 func GetHand(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("htmlTemplates/work.html"))
-	pictures := map[string][]Picture{
-		"Pictures": {
-			{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg", PicID: 1},
-			{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 2},
-			{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 3},
-			{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 4},
-			{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg",PicID: 5},
-			{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 6},
-			{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 7},
-			{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 8},
-			{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg", PicID: 9},
-			{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 10},
-			{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 11},
-			{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 12},
-		},
-	}
+	pictures := WorksMap
+
 	tmpl.Execute(w, pictures)
 }
 func PostHand(w http.ResponseWriter, r *http.Request) {
@@ -93,31 +98,28 @@ func PostHand(w http.ResponseWriter, r *http.Request) {
 
 func GetHandEditor(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("htmlTemplates/editorTemplates/workEditor.html"))
-	pictures:= map[string][]Picture{
-		"Pictures": {
-			{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg", PicID: 1},
-			{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 2},
-			{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 3},
-			{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 4},
-			{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg",PicID: 5},
-			{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 6},
-			{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 7},
-			{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 8},
-			{Title: "BEACH", Path: "../static/images/userWorks/beach.jpg", PicID: 9},
-			{Title: "FOREST - EL YUNQUE", Path: "../static/images/userWorks/forest.jpg", PicID: 10},
-			{Title: "ICELAND - BLACK SANDS", Path: "../static/images/userWorks/iceland.jpg", PicID: 11},
-			{Title: "FOOD", Path: "../static/images/userWorks/food.jpg", PicID: 12},
-		},
-	}
+	pictures := WorksMap
 	tmpl.Execute(w, pictures)
 }
 
 // Working here i need to know what component was activated edit title, delete pic, etc
 func PostHandEditor(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Fetching EditorTitle component")
+	// Read the body of the request
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Could not read body")
+		http.Error(w, "Failed to read body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// Print the body content
+	fmt.Println("Body:", string(body))
+	r.Body = io.NopCloser(io.MultiReader(bytes.NewReader(body)))
 
 	// Extract values from request
-	err := r.ParseForm()
+	err = r.ParseForm()
 	if err != nil {
 		http.Error(w, "Unable to parse form", http.StatusBadRequest)
 		return
@@ -157,7 +159,6 @@ func FectchComponent(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Fecthing back buttonsEditor")
 
 	err := r.ParseForm()
-	
 	if err != nil {
 		http.Error(w, "Unable to parse form", http.StatusBadRequest)
 		return
@@ -172,4 +173,50 @@ func FectchComponent(w http.ResponseWriter, r *http.Request){
 
 	tmpl := template.Must(template.ParseFiles("htmlTemplates/components/buttonsEditor.html"))
 	tmpl.Execute(w, Data)
+}
+
+// Working here-------------------------------------------------------------------------------------------------------
+// PUTS ---------- maybe put into other folders this------------------------------------------------------------------
+
+func PutHandEditor(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PUT EDITOR")
+	err:= r.ParseForm()
+	if err!= nil{
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return 
+	}
+
+	PicID := r.FormValue("PicID")
+	if PicID == ""{
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+	PicIDint,_ := strconv.Atoi(PicID)
+	Title := r.FormValue("Title")
+
+	PicBytes, _, err := r.FormFile("picture")
+	if err != nil {
+		fmt.Println("No picture detected")
+	} else {
+		defer PicBytes.Close() // Prevent resource leak
+	}
+	
+	if PicBytes==nil && Title!=""{
+		fmt.Println("EDIT TITLE FOR PIC")
+		for key,works := range WorksMap{
+			for work := range works{
+				if works[work].PicID == PicIDint{
+					fmt.Println("Found a match for the id")
+					WorksMap[key][work].Title = Title
+					// http://localhost:8000/work/editor
+					//  need to redirect url
+					http.Redirect(w, r, "/work/editor", http.StatusSeeOther)
+				}
+			}
+		}
+	}else if  PicBytes!=nil && Title==""{
+		fmt.Println("Change Picture")
+	}else{
+		fmt.Println("No data sent")
+	}
 }
