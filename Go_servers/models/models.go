5acc94f1be15f077c771f51166e254fe69159b61
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-08-28 01:08:50 -0400

    ADD - New strcut for galleries tabel from suapabase that was added.

--- Go_servers/models/models.go
+++ Go_servers/models/models.go
@@ -1,21 +1,38 @@
 /*All base struct models and storage for structs*/
 package models
 
-// Use this for query on "works" table
+// Use this for query on "works" table. Representation of the "works" table
 type Work struct {
+	Id int `json:"ID"`
 	Path   string `json:"Path"`
 	Title     string `json:"Title"`
 	Description string `json:"Description"`
 	Position int    `json:"Position"`
 }
+// Use this for query on "galleries" table. Representation of the "galleries" table
+type GalleryItem struct{
+	Path string `json:"Path"`
+	Position int `json:"Position"`
+	Work_ID int `json:"Work_ID"` // Reference key to table "works"
+}
 
-// Need this struct to send strings for front end HTML content, its the same as work but only strings
+
+// Need this structs to send strings for front end HTML content.
 type WorkFrontEnd struct{
 	Path   string `json:"Path"`
 	Title     string `json:"Title"`
 	Description string `json:"Description"`
 	Position string    `json:"Position"`
+	PositionBelow string `json:"PositionBelow"`
 }
 
-/* Local storage for works*/
+type GalleryItemFrontEnd struct{
+	Path string `json:"Path"`
+	Position string `json:"Position"`
+}
+
+/* Local storage for works. <work.Path> is storing the public bucket url, not the just the picture name*/
 var WorksStorage []Work
+
+/* Local storage for galleries assocciate with works_id and []Gallery. <Gallery.Path> is storing the public bucket url, not the the picture name*/
+var GalleriesStorage map[int][]GalleryItem
