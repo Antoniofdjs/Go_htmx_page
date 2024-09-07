3b13ad523da6630d7c6c58953d05e5265961b790
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-09-07 18:44:21 -0400

    ADD - Map for works, able to find a work based on the title

--- Go_servers/models/models.go
+++ Go_servers/models/models.go
@@ -1,4 +1,8 @@
-/*All base struct models and storage for structs*/
+/*
+All base struct models and storage for structs
+IMPORTANT local storages for Object.path contains the url for the picture
+In the database, "Path" is the picture name, not the same
+*/
 package models
 
 // Use this for query on "works" table. Representation of the "works" table
@@ -9,11 +13,15 @@ type Work struct {
 	Description string `json:"Description"`
 	Position int    `json:"Position"`
 }
+
+var WorksMapStorage map[string]Work // Find works based on the title
+
 // Use this for query on "galleries" table. Representation of the "galleries" table
 type GalleryItem struct{
 	Path string `json:"Path"`
 	Position int `json:"Position"`
 	Work_ID int `json:"Work_ID"` // Reference key to table "works"
+	ID int `json:"id"`
 }
 
 
@@ -37,9 +45,14 @@ var WorksStorage []Work
 /* Local storage for galleries assocciate with works_id and []Gallery. <Gallery.Path> is storing the public bucket url, not the the picture name*/
 var GalleriesStorage map[int][]GalleryItem
 
+// Contains picture name(file name) and the bytes for files that will be uploaded
 type FileTemp struct{
 	FileName string
 	FileBytes []byte
 }
 
+// Local storage temp for uploaded picture files
 var FileTempStorage []FileTemp
+
+// Store the work title and positions of gallery items to delete
+var DeleteGalleryItemTempStorage map[string][]int
